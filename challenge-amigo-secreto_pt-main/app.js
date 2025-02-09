1bd6e5411b44dc9fcc5b6f25067a4bb9aeb2c05f
Heber-yuri <heberbiro1234@gmail.com>
2025-02-09 19:47:47 -0300

    função para agregar amigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+    let amigos = [];
+
+    function adicionarAmigo() {
+        let nome = document.querySelector('input').value;
+
+        if (nome !== "" && !amigos.includes(nome)) {
+            amigos.push(nome);
+            nome = document.querySelector('input');
+            nome.value = '';
+    
+        } else {
+            alert("Digite um nome válido e não repetido.");
+        }
+    }
